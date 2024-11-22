7e46c97035bf29be8f021182a645ee8b391dd127
deAmber <damberdev98@gmail.com>
2024-11-23 08:52:10 +1100

    test publish

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://deamber.github.io/media-history/",
   resolve: {
     alias: {
       '@css': path.resolve(__dirname, './src/css'),
